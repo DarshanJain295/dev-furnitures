07c61c90e6bf40622792327c6755a41a61b1ea83
Darshan Jain <jaindarshan295@gmail.com>
2022-07-27 15:28:28 +0530

    Updates

---
+++ precache-manifest.264e3e996434a2455647a0aecedd31dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5051f34cdbd6d51c15d0",
+    "url": "/dev-furnitures/static/css/main.d9df89e7.chunk.css"
+  },
+  {
+    "revision": "5051f34cdbd6d51c15d0",
+    "url": "/dev-furnitures/static/js/main.9b7a461e.chunk.js"
+  },
+  {
+    "revision": "1834b451f94178186f4b",
+    "url": "/dev-furnitures/static/js/runtime~main.35b62816.js"
+  },
+  {
+    "revision": "4f1e05d44f438c197f94",
+    "url": "/dev-furnitures/static/css/2.3899326e.chunk.css"
+  },
+  {
+    "revision": "4f1e05d44f438c197f94",
+    "url": "/dev-furnitures/static/js/2.af15f456.chunk.js"
+  },
+  {
+    "revision": "c80db57e9a2be3dff255c497ea20377a",
+    "url": "/dev-furnitures/static/media/image1.c80db57e.jpg"
+  },
+  {
+    "revision": "0590fc2773beed99cfd3f5b6ff8e7de5",
+    "url": "/dev-furnitures/static/media/image2.0590fc27.jpg"
+  },
+  {
+    "revision": "d55fd1101f6642205db70d1d5df2e465",
+    "url": "/dev-furnitures/static/media/image3.d55fd110.jpg"
+  },
+  {
+    "revision": "79e6997f498d53f190e1fc926be47c0f",
+    "url": "/dev-furnitures/static/media/image4.79e6997f.jpg"
+  },
+  {
+    "revision": "d76da0d14a63854e854249a8567d21a8",
+    "url": "/dev-furnitures/static/media/image5.d76da0d1.jpg"
+  },
+  {
+    "revision": "49d392c96bad211e80cedeff210863de",
+    "url": "/dev-furnitures/static/media/image6.49d392c9.jpg"
+  },
+  {
+    "revision": "b34d67b3897b36aecb296ebe1e315de6",
+    "url": "/dev-furnitures/index.html"
+  }
+];
